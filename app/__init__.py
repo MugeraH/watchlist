bc8e2e04d2ce499253b281e6036ac2f085bd903b
MugeraH <bwosihughes24@gmail.com>
2021-04-13 04:18:42 +0300

    add request.py file to make requests to our api

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,10 @@ from flask import Flask
 from .config import DevConfig
 
 #initalizing application
-app = Flask(__name__)
+app = Flask(__name__,instance_relative_config=True)
 
 #setting up configuration
 app.config.from_object(DevConfig)
+aoo.config.from_pyfile('config.py')
 
 from app import views
---
+++ app/request.py
@@ -0,0 +1,4 @@
+from app import app
+
+#Getting api_key
+api_key = app.config['MOVIE_API_KEY']
