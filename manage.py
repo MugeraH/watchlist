81fd28bc8ea2faac2c2cd16fdb1f2ef884e850e5
MugeraH <bwosihughes24@gmail.com>
2021-04-20 06:52:00 +0300

    add roles table,define one to many relationship with users table

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@ from app import create_app,db
 from flask_script import Manager,Server
 
 
-from app.models import User
+from app.models import User,Role
 
 
 
@@ -21,7 +21,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User)
+    return dict(app = app,db = db,User = User,Role = Role)
 
 
 if __name__ == '__main__':
